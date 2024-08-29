e5267500924f7f217eb95ca0ba051756aa92a9c5
Ben Wirf <ben.wirf@gmail.com>
2024-08-29 20:23:30 +0930

    Add files via upload

--- Processing_scripts/Save_color_map_to_csv.py
+++ Processing_scripts/Save_color_map_to_csv.py
@@ -1,13 +1,16 @@
 from qgis.PyQt.QtCore import QCoreApplication, QVariant
+from qgis.PyQt.QtWidgets import QDialog, QTabWidget
 from qgis.core import (QgsProcessing, QgsProcessingAlgorithm,
                         QgsProcessingParameterVectorLayer,
                         QgsProcessingParameterFileDestination)
 
+from qgis.utils import iface
 import csv
 
 
 class ExportColorMapToCsv(QgsProcessingAlgorithm):
     LAYER = 'LAYER'
+    COLUMN_HEADERS = 'COLUMN_HEADERS'
     OUTPUT_CSV_PATH = 'OUTPUT_CSV_PATH'
  
     def __init__(self):
@@ -27,8 +30,8 @@ class ExportColorMapToCsv(QgsProcessingAlgorithm):
  
     def shortHelpString(self):
         return "Export a color map from a categorised vector layer to a CSV file\
-                The CSV will contain the renderer legend labels and the hex codes\
-                of the associated category colors."
+                The CSV will contain the renderer class values, hex codes and\
+                RGB values of the associated category colors."
  
     def helpUrl(self):
         return "https://qgis.org"
@@ -45,14 +48,32 @@ class ExportColorMapToCsv(QgsProcessingAlgorithm):
             "Input layer",
             [QgsProcessing.TypeVectorAnyGeometry]))
             
-        self.addParameter()
+        self.addParameter(QgsProcessingParameterFileDestination(
+            self.OUTPUT_CSV_PATH,
+            "Output CSV file",
+            "*.csv"))
 
  
     def processAlgorithm(self, parameters, context, feedback):
         layer = self.parameterAsVectorLayer(parameters, self.LAYER, context)
+        result_csv = self.parameterAsFileOutput(parameters, self.OUTPUT_CSV_PATH, context)
+        ################################
+        csv_tbl = open(result_csv, mode='w', newline='')
+        writer = csv.writer(csv_tbl)
+        r = layer.renderer()
+        if not isinstance(r, QgsCategorizedSymbolRenderer):
+            return {}
+        class_attribute = r.classAttribute()
+        writer.writerow([class_attribute, 'Hex Code', 'RGB Values'])
+        for cat in r.categories():
+            ls = cat.value()
+            col = cat.symbol().color()
+            hex_code = col.name()
+            writer.writerow([ls, hex_code])
+        csv_tbl.close()
+        ################################
  
- 
-        return {}
+        return {'Color map CSV': result_csv}
         
     def postProcessAlgorithm(self, context, feedback):
         # hack to work around ?bug where, if algorithm returns the NoThreading flag,
