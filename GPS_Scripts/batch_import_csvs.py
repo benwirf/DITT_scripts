566686837310cafc485c9fa1785a8e80c23f2ecd
Ben Wirf <ben.wirf@gmail.com>
2025-03-21 13:31:26 +0930

    Update batch_import_csvs.py

--- GPS_Scripts/batch_import_csvs.py
+++ GPS_Scripts/batch_import_csvs.py
@@ -1,15 +1,15 @@
 from pathlib import Path
 import os
 
-src_folder = 'C:\\Users\\qw2\\Desktop\\Paddock_Power_GPS\\GPS Data Aug21-March22\\CSVs'
+src_folder = 'Paddock_Power_GPS\\GPS Data Aug21-March22\\CSVs'
 
 for file in os.scandir(src_folder):
 #    print(file.name)
     stem = Path(file).stem
 #    print(stem)
-    out_path = f'C:/Users/qw2/Desktop/Paddock_Power_GPS/GPS Data Aug21-March22/gpkgs/{stem}.gpkg'
+    out_path = f'Paddock_Power_GPS/GPS Data Aug21-March22/gpkgs/{stem}.gpkg'
     
-    save_params = {'INPUT':f'delimitedtext://file:///C:/Users/qw2/Desktop/Paddock_Power_GPS/GPS%20Data%20Aug21-March22/CSVs/{file.name}?type=csv&maxFields=10000&detectTypes=yes&xField=Longitude&yField=Latitude&crs=EPSG:4326&spatialIndex=no&subsetIndex=no&watchFile=no',
+    save_params = {'INPUT':f'delimitedtext://file:///Paddock_Power_GPS/GPS%20Data%20Aug21-March22/CSVs/{file.name}?type=csv&maxFields=10000&detectTypes=yes&xField=Longitude&yField=Latitude&crs=EPSG:4326&spatialIndex=no&subsetIndex=no&watchFile=no',
                     'OUTPUT':out_path,
                     'LAYER_NAME':'',
                     'DATASOURCE_OPTIONS':'',
