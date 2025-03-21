43259e582d8c1d2a7266151fd32a03259d4f01a3
Ben Wirf <ben.wirf@gmail.com>
2025-03-21 13:01:14 +0930

    Update Export_checked_layers_to_spreadsheet.py

--- Export_checked_layers_to_spreadsheet.py
+++ Export_checked_layers_to_spreadsheet.py
@@ -7,7 +7,7 @@ lyrs = [l.layer() for l in root.findLayers() if l.isVisible()]
 params = {'LAYERS':lyrs,
             'USE_ALIAS':False,
             'FORMATTED_VALUES':False,
-            'OUTPUT':'C:/Users/qw2/Desktop/Mulga_Park/For_Robyn/Mulga_Park_RRR.xlsx',
+            'OUTPUT':'/Mulga_Park/For_xxx/Mulga_Park_RRR.xlsx',
             'OVERWRITE':False}
 
 processing.run("native:exporttospreadsheet", params)
