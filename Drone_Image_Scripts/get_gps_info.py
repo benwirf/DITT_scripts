5da02823af0088a7f9fd34c903ba2b771c7d28f2
Ben Wirf <ben.wirf@gmail.com>
2025-03-21 13:35:58 +0930

    Update get_gps_info.py

--- Drone_Image_Scripts/get_gps_info.py
+++ Drone_Image_Scripts/get_gps_info.py
@@ -7,7 +7,7 @@ from PIL.ExifTags import GPSTAGS
 # print(TAGS)
 # print(GPSTAGS)
 
-folder_path = 'C:\\Users\\Ben\\Desktop\\gate-pdk_drone\\DJI_202305191306_003_gate-pdk1'
+folder_path = 'gate-pdk_drone\\DJI_202305191306_003_gate-pdk1'
 folder_files = [file for file in os.scandir(folder_path)]
 rgb_images = [f for f in folder_files if f.name.split('.')[1] == 'JPG']
 image_file = rgb_images[0]
