e39e8230d90fa5d92b2bc976b3421c7c6ccac656
Ben Wirf <ben.wirf@gmail.com>
2025-03-21 13:37:55 +0930

    Update homerange_cibo_zonal_stats_table.py

--- Paddock_Power_GPS_Data_Analysis/homerange_cibo_zonal_stats_table.py
+++ Paddock_Power_GPS_Data_Analysis/homerange_cibo_zonal_stats_table.py
@@ -6,7 +6,7 @@ months_dict = dict((month, index) for index, month in enumerate(calendar.month_n
 
 homerange_source_folder = r'Paddock_Power_GPS/home_range_geopackage_files'
 
-cibo_tsdm_folder = r'C:/Users/qw2/Desktop/Paddock_Power_GPS/Cibo_TSDM'
+cibo_tsdm_folder = r'Paddock_Power_GPS/Cibo_TSDM'
 
 period_1_months = ['June', 'July', 'August', 'September', 'October', 'November', 'December']
 period_2_months = ['January', 'February', 'March', 'April', 'May']
