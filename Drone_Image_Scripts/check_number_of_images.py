9fbc998bd585ef953e174f9584d3edc7033f7d20
Ben Wirf <ben.wirf@gmail.com>
2025-03-21 13:33:48 +0930

    Update check_number_of_images.py

--- Drone_Image_Scripts/check_number_of_images.py
+++ Drone_Image_Scripts/check_number_of_images.py
@@ -1,4 +1,4 @@
-folder_path = 'C:\\Users\\Ben\\Desktop\\gate-pdk_drone\\DJI_202305191306_003_gate-pdk1'
+folder_path = 'gate-pdk_drone\\DJI_202305191306_003_gate-pdk1'
 folder_files = [file for file in os.scandir(folder_path)]
 rgb_images = [f for f in folder_files if f.name.split('.')[1] == 'JPG']
 print(len(rgb_images))
