307eee7f3ca01ef49ca05e7bcf8a0c9f626ca508
Ben Wirf <ben.wirf@gmail.com>
2025-03-21 13:04:18 +0930

    Update Raster_file_writer.py

--- Raster_file_writer.py
+++ Raster_file_writer.py
@@ -6,7 +6,7 @@ pipe = QgsRasterPipe()
 project = QgsProject().instance()
 
 for layer in layers:
-#    file_name = os.path.join('P:\\VSS_SHARE\\01_PROJECTS\\IMAGERY\\AIM_POINTS\\MATERIAL\\ELLSWORTH_REQUEST\\DEV', f'{layer.name()}.tif')
+#    file_name = os.path.join('Path\to\raster', f'{layer.name()}.tif')
     file_name = os.path.join('C:\\Users\\qw2\\Desktop\\test', f'{layer.name()}_rendered.tif')
     pipe.set(layer.dataProvider().clone())
     pipe.set(layer.renderer().clone())
